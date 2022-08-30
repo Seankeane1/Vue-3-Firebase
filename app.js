bd61baebce81da6a68135403a4a88e28c2f59010
SeanKeane1 <seankean36@gmail.com>
2022-08-30 12:22:48 -0400

    Video 3

--- app.js
+++ app.js
@@ -3,29 +3,28 @@ const app = Vue.createApp({
     //template: '<h2>I am the template</h2>'
     data(){
         return{
+            url: 'http://www.thenetninja.co.uk',
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0,
+            books: [
+                {title: 'name of the wind', author: 'patrick rothfuss', isFav: false},
+                {title: 'the way of kings', author: 'brandon sanderson', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', isFav: true}
+            ]
         }
     },
     methods: {
         toggleShowBooks(){
             this.showBooks = !this.showBooks; // sets it to whatever it currently isn't
         },
-        handleEvent(e, data){
-            console.log(e)
-            if (data){
-                console.log(data);
-            }
-        },
-        handleMouseMove(e){
-            this.x = e.offsetX
-            this.y = e.offsetY
+        toggleFav(book){
+            book.isFav = !book.isFav
         }
-    }
+    },
+        computed: { //depends on other data
+            filteredBooks(){
+                return this.books.filter((book) => book.isFav)
+            }
+        } 
 })
 
 app.mount('#app') // injects this element into the id 'app'
